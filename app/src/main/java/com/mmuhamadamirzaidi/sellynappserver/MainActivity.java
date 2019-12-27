ec420a0c18b08416944d9e3fa18e7cc5e63483ef
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-12-28 07:01:57 +0800

    In Progress Create New Product

--- app/src/main/java/com/mmuhamadamirzaidi/sellynappserver/MainActivity.java
+++ app/src/main/java/com/mmuhamadamirzaidi/sellynappserver/MainActivity.java
@@ -2,7 +2,6 @@ package com.mmuhamadamirzaidi.sellynappserver;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,7 +23,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -75,11 +73,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     SwipeRefreshLayout swipe_layout_category;
 
-    FloatingActionButton fab;
-
     EditText add_category_name;
 
     ImageView add_image_category;
+
+    FloatingActionButton add_category;
 
     Button button_update, button_cancel;
 
@@ -138,8 +136,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        add_category = findViewById(R.id.add_category);
+        add_category.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mainIntent = new Intent(MainActivity.this, AddCategoryActivity.class);
@@ -196,9 +194,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     public void onClick(View view, int position, boolean isLongClick) {
 
                         // Get CategoryId and send to new activity
-//                        Intent product_id = new Intent(MainActivity.this, ProductListActivity.class);
-//                        product_id.putExtra("categoryId", adapter.getRef(position).getKey());
-//                        startActivity(product_id);
+                        Intent product_id = new Intent(MainActivity.this, ProductListActivity.class);
+                        product_id.putExtra("categoryId", adapter.getRef(position).getKey());
+                        startActivity(product_id);
 
                         Toast.makeText(MainActivity.this, "Category ID" + adapter.getRef(position).getKey(), Toast.LENGTH_SHORT).show();
                     }
---
+++ app/src/main/java/com/mmuhamadamirzaidi/sellynappserver/Model/Product.java
@@ -0,0 +1,84 @@
+package com.mmuhamadamirzaidi.sellynappserver.Model;
+
+public class Product {
+
+    private String productName, productImage, productDescription, productPrice, productDiscount, productHolder, notificationNo, categoryId;
+
+    public Product() {
+    }
+
+    public Product(String productName, String productImage, String productDescription, String productPrice, String productDiscount, String productHolder, String notificationNo, String categoryId) {
+        this.productName = productName;
+        this.productImage = productImage;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.productDiscount = productDiscount;
+        this.productHolder = productHolder;
+        this.notificationNo = notificationNo;
+        this.categoryId = categoryId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDiscount() {
+        return productDiscount;
+    }
+
+    public void setProductDiscount(String productDiscount) {
+        this.productDiscount = productDiscount;
+    }
+
+    public String getProductHolder() {
+        return productHolder;
+    }
+
+    public void setProductHolder(String productHolder) {
+        this.productHolder = productHolder;
+    }
+
+    public String getNotificationNo() {
+        return notificationNo;
+    }
+
+    public void setNotificationNo(String notificationNo) {
+        this.notificationNo = notificationNo;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+}
